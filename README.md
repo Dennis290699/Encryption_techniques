a6f329ded5f42c9bd1df78a469f40129a0b7a5cc
Dennis290699 <trujilloadrian29@gmail.com>
2023-06-20 08:27:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,81 +1,83 @@
-# Web Encryption and Decryption
+# Cifrado y Descifrado Web
 
-This is a project for a simple web page that provides functionality for encryption and decryption using the methods of Caesar Cipher and Affine Cipher. The page is developed with HTML, CSS, and JavaScript.
+Este es un proyecto de una página web simple que proporciona funcionalidad para el cifrado y descifrado utilizando los métodos de Cifrado César y Cifrado Afín. La página está desarrollada con HTML, CSS y JavaScript.
 
-## Features
+## Características
 
-- Encryption and decryption using the Caesar Cipher and Affine Cipher methods.
-- Customization of the shift for the Caesar Cipher and the keys for the Affine Cipher.
-- Switch between light and dark themes.
-- User-friendly and responsive interface.
+- Cifrado y descifrado utilizando el método de Cifrado César y Cifrado Afín.
+- Personalización del desplazamiento para el Cifrado César y las claves para el Cifrado Afín.
+- Cambio entre temas claro y oscuro.
+- Interfaz de usuario amigable y responsive.
 
 ## Demo
 
-You can see a live demo of the project [here](https://dennis290699.github.io/Encryption_techniques/).
+Puedes ver una demostración en vivo del proyecto [aquí](https://dennis290699.github.io/Encryption_techniques/).
 
-## Screenshots
+## Capturas de Pantalla
 
-### Light Theme
+### Tema Claro
 
-<img src="./assets/theme-light.png" alt="Screenshot - Light Theme" width="300">
+<img src="./assets/theme-light.png" alt="Captura de pantalla - Tema Claro" width="300">
 
-This screenshot shows the page with the light theme applied. The predominant colors are soft and bright, providing a pleasant and luminous appearance. The interface elements are easily distinguishable and adapt well to a light background.
+Esta captura de pantalla muestra la página con el tema claro aplicado. Los colores predominantes son suaves y claros, lo que proporciona una apariencia agradable y luminosa. Los elementos de la interfaz son fácilmente distinguibles y se adaptan a un fondo claro. 
 
-### Dark Theme
+### Tema Oscuro
 
-<img src="./assets/theme-dark.png" alt="Screenshot - Dark Theme" width="300">
+<img src="./assets/theme-dark.png" alt="Captura de pantalla - Tema Oscuro" width="300">
 
-In this screenshot, the page is shown with the dark theme enabled. The colors used are darker and adjust to a dark background, providing an elegant and modern appearance. The text and elements stand out thanks to the contrasting colors. The dark theme can also help reduce visual fatigue in low-light environments.
+En esta captura de pantalla, se muestra la página con el tema oscuro activado. Los colores utilizados son más oscuros y se ajustan a un fondo oscuro, lo que proporciona una apariencia elegante y moderna. El texto y los elementos resaltan gracias a los colores contrastantes. El tema oscuro también puede ayudar a reducir la fatiga visual en entornos de poca luz.
 
-## Installation
 
-1. Clone this repository to your local machine:
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local:
 
    ```bash
    git clone https://github.com/Dennis290699/Encryption_techniques.git
    ```
 
-2. Navigate to the project directory:
+2. Navega hasta el directorio del proyecto:
 
    ```bash
-   cd repository-name
+   cd nombre-del-repo
    ```
 
-3. Open the `index.html` file in your web browser.
+3. Abre el archivo `index.html` en tu navegador web.
 
-## Customization
+## Personalización
 
-You can customize the project according to your needs:
+Puedes personalizar el proyecto según tus necesidades:
 
-- Modify the design and styles in the CSS file (`style.css`).
-- Add more encryption and decryption methods in the JavaScript file (`script.js`).
-- Add more customization options in the HTML file (`index.html`).
+- Modifica el diseño y los estilos en el archivo CSS (`style.css`).
+- Añade más métodos de cifrado y descifrado en el archivo JavaScript (`script.js`).
+- Añade más opciones de personalización en el archivo HTML (`index.html`).
 
-## Contribution
+## Contribución
 
-If you want to contribute to this project, follow these steps:
+Si deseas contribuir a este proyecto, sigue estos pasos:
 
-1. Fork this repository.
-2. Create a new branch for your changes:
+1. Haz un fork de este repositorio.
+2. Crea una nueva rama para tus cambios:
 
    ```bash
-   git checkout -b feature/new-feature
+   git checkout -b feature/nueva-funcionalidad
    ```
 
-3. Make your changes and commit them:
+3. Realiza tus cambios y haz commits:
 
    ```bash
-   git commit -m "Add new feature"
+   git commit -m "Agrega nueva funcionalidad"
    ```
 
-4. Push your changes to your forked repository:
+4. Empuja tus cambios a tu repositorio fork:
 
    ```bash
-   git push origin feature/new-feature
+   git push origin feature/nueva-funcionalidad
    ```
 
-5. Open a pull request in this repository.
+5. Abre un pull request en este repositorio.
 
-## License
+## Licencia
 
-This project is licensed under the [MIT License](LICENSE).
+Este proyecto está licenciado bajo la [Licencia MIT](LICENSE).
--- js/script.js
+++ js/script.js
@@ -1,138 +1,123 @@
 function toggleOptions() {
-    const cipherSelect = document.getElementById("cipher");
-    const cesarOptions = document.getElementById("cesar-options");
-    const afinOptions = document.getElementById("afin-options");
-  
-    cesarOptions.classList.add("hidden");
-    afinOptions.classList.add("hidden");
-  
-    if (cipherSelect.value === "cesar") {
-      cesarOptions.classList.remove("hidden");
-    } else if (cipherSelect.value === "afin") {
-      afinOptions.classList.remove("hidden");
-    }
+  const cipherSelect = document.getElementById("cipher");
+  const cesarOptions = document.getElementById("cesar-options");
+  const afinOptions = document.getElementById("afin-options");
+
+  cesarOptions.classList.add("hidden");
+  afinOptions.classList.add("hidden");
+
+  if (cipherSelect.value === "cesar") {
+    cesarOptions.classList.remove("hidden");
+  } else if (cipherSelect.value === "afin") {
+    afinOptions.classList.remove("hidden");
   }
-  
-  // Llamar a la función toggleOptions() al cargar la página para ocultar las opciones por defecto
-  toggleOptions();  
+}
+
+// Llamar a la función toggleOptions() al cargar la página para ocultar las opciones por defecto
+window.addEventListener("load", toggleOptions);
 
 function encrypt() {
-    var message = document.getElementById("message").value;
-    var cipherType = document.getElementById("cipher").value;
-  
-    var encryptedMessage = "";
-  
-    if (cipherType === "caesar") {
-      var shift = parseInt(document.getElementById("shift").value);
-  
-      for (var i = 0; i < message.length; i++) {
-        var charCode = message.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) { // Uppercase letters
-          encryptedMessage += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
-          encryptedMessage += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
-        } else {
-          encryptedMessage += message.charAt(i); // Non-alphabetic characters
-        }
-      }
-    } else if (cipherType === "affine") {
-      var keyA = parseInt(document.getElementById("keyA").value);
-      var keyB = parseInt(document.getElementById("keyB").value);
-  
-      for (var i = 0; i < message.length; i++) {
-        var charCode = message.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) { // Uppercase letters
-          encryptedMessage += String.fromCharCode(((keyA * (charCode - 65) + keyB) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
-          encryptedMessage += String.fromCharCode(((keyA * (charCode - 97) + keyB) % 26) + 97);
-        } else {
-          encryptedMessage += message.charAt(i); // Non-alphabetic characters
-        }
-      }
-    }
-  
-    document.getElementById("result").value = encryptedMessage;
-  }
-  
-  function decrypt() {
-    var message = document.getElementById("message").value;
-    var cipherType = document.getElementById("cipher").value;
-  
-    var decryptedMessage = "";
-  
-    if (cipherType === "caesar") {
-      var shift = parseInt(document.getElementById("shift").value);
-  
-      for (var i = 0; i < message.length; i++) {
-        var charCode = message.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) { // Uppercase letters
-          decryptedMessage += String.fromCharCode(((charCode - 65 - shift + 26) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
-          decryptedMessage += String.fromCharCode(((charCode - 97 - shift + 26) % 26) + 97);
-        } else {
-          decryptedMessage += message.charAt(i); // Non-alphabetic characters
-        }
-      }
-    } else if (cipherType === "affine") {
-      var keyA = parseInt(document.getElementById("keyA").value);
-      var keyB = parseInt(document.getElementById("keyB").value);
-  
-      // Calculate modular multiplicative inverse of keyA
-      var modInverse = 0;
-      for (var i = 0; i < 26; i++) {
-        if ((keyA * i) % 26 === 1) {
-          modInverse = i;
-          break;
-        }
-      }
-  
-      for (var i = 0; i < message.length; i++) {
-        var charCode = message.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) { // Uppercase letters
-          var decodedCharCode = ((modInverse * (charCode - 65 - keyB + 26)) % 26) + 65;
-          decryptedMessage += String.fromCharCode(decodedCharCode);
-        } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
-          var decodedCharCode = ((modInverse * (charCode - 97 - keyB + 26)) % 26) + 97;
-          decryptedMessage += String.fromCharCode(decodedCharCode);
-        } else {
-          decryptedMessage += message.charAt(i); // Non-alphabetic characters
-        }
-      }
-    }
-  
-    document.getElementById("result").value = decryptedMessage;
-  }
-  
-  // Mostrar u ocultar opciones según el tipo de cifrado seleccionado
-  document.getElementById("cipher").addEventListener("change", function() {
-    var cipherType = document.getElementById("cipher").value;
-    var caesarOptions = document.getElementById("caesar-options");
-    var affineOptions = document.getElementById("affine-options");
-  
-    if (cipherType === "caesar") {
-      caesarOptions.style.display = "block";
-      affineOptions.style.display = "none";
-    } else if (cipherType === "affine") {
-      caesarOptions.style.display = "none";
-      affineOptions.style.display = "block";
-    }
-  });
-  
-  function clearFields() {
-    document.getElementById("message").value = ""; // Limpiar campo de mensaje
-    document.getElementById("shift").value = ""; // Limpiar campo de desplazamiento
-    document.getElementById("keyA").value = ""; // Limpiar campo de clave A
-    document.getElementById("keyB").value = ""; // Limpiar campo de clave B
-    document.getElementById("result").value = ""; // Limpiar campo de resultado
-    
-    document.getElementById("cipher").selectedIndex = 0; // Restablecer selección del tipo de cifrado
-    toggleOptions(); // Ocultar las opciones de cifrado
-  }
-   
+  var message = document.getElementById("message").value.toUpperCase();
+  var cipherType = document.getElementById("cipher").value;
 
-  function toggleTheme() {
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.toggle("dark-theme");
+  var encryptedMessage = "";
+
+  if (cipherType === "cesar") {
+    var shift = parseInt(document.getElementById("shift").value);
+
+    for (var i = 0; i < message.length; i++) {
+      var charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+        encryptedMessage += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+      } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+        encryptedMessage += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+      } else {
+        encryptedMessage += message.charAt(i); // Non-alphabetic characters
+      }
+    }
+  } else if (cipherType === "afin") {
+    var keyA = parseInt(document.getElementById("keyA").value);
+    var keyB = parseInt(document.getElementById("keyB").value);
+
+    for (var i = 0; i < message.length; i++) {
+      var charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+        encryptedMessage += String.fromCharCode(((keyA * (charCode - 65) + keyB) % 26) + 65);
+      } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+        encryptedMessage += String.fromCharCode(((keyA * (charCode - 97) + keyB) % 26) + 97);
+      } else {
+        encryptedMessage += message.charAt(i); // Non-alphabetic characters
+      }
+    }
   }
-  
-  
+
+  document.getElementById("result").value = encryptedMessage;
+}
+
+function decrypt() {
+  var message = document.getElementById("message").value.toUpperCase();
+  var cipherType = document.getElementById("cipher").value;
+
+  var decryptedMessage = "";
+
+  if (cipherType === "cesar") {
+    var shift = parseInt(document.getElementById("shift").value);
+
+    for (var i = 0; i < message.length; i++) {
+      var charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+        decryptedMessage += String.fromCharCode(((charCode - 65 - shift + 26) % 26) + 65);
+      } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+        decryptedMessage += String.fromCharCode(((charCode - 97 - shift + 26) % 26) + 97);
+      } else {
+        decryptedMessage += message.charAt(i); // Non-alphabetic characters
+      }
+    }
+  } else if (cipherType === "afin") {
+    var keyA = parseInt(document.getElementById("keyA").value);
+    var keyB = parseInt(document.getElementById("keyB").value);
+
+    // Calculate modular multiplicative inverse of keyA
+    var modInverse = 0;
+    for (var i = 0; i < 26; i++) {
+      if ((keyA * i) % 26 === 1) {
+        modInverse = i;
+        break;
+      }
+    }
+
+    for (var i = 0; i < message.length; i++) {
+      var charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+        var decodedCharCode = ((modInverse * (charCode - 65 - keyB + 26)) % 26) + 65;
+        decryptedMessage += String.fromCharCode(decodedCharCode);
+      } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+        var decodedCharCode = ((modInverse * (charCode - 97 - keyB + 26)) % 26) + 97;
+        decryptedMessage += String.fromCharCode(decodedCharCode);
+      } else {
+        decryptedMessage += message.charAt(i); // Non-alphabetic characters
+      }
+    }
+  }
+
+  document.getElementById("result").value = decryptedMessage;
+}
+
+// Mostrar u ocultar opciones según el tipo de cifrado seleccionado
+document.getElementById("cipher").addEventListener("change", toggleOptions);
+
+function clearFields() {
+  document.getElementById("message").value = ""; // Limpiar campo de mensaje
+  document.getElementById("shift").value = ""; // Limpiar campo de desplazamiento
+  document.getElementById("keyA").value = ""; // Limpiar campo de clave A
+  document.getElementById("keyB").value = ""; // Limpiar campo de clave B
+  document.getElementById("result").value = ""; // Limpiar campo de resultado
+
+  document.getElementById("cipher").selectedIndex = 0; // Restablecer selección del tipo de cifrado
+  toggleOptions(); // Ocultar las opciones de cifrado
+}
+
+function toggleTheme() {
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.toggle("dark-theme");
+}
